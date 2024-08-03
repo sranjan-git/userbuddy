4c8ee39fa47f37534a8c5b9e59f0e1be9ad2e2be
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2024-08-04 00:21:55 +0530

    Update api.ts

--- client /src/services/api.ts
+++ client /src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.withCredentials = true;
 const api = axios.create({
-  baseURL: 'https://userbuddy.vercel.app',
+  baseURL: 'http://localhost:5001/',
 });
 
 export default api;
